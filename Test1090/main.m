c182f2fe0276d4f32352cfd97432e6cec794ba79
Craig Bennett <craigo@gmail.com>
2015-08-15 23:17:36 -0700

    Main commit

---
+++ Test1090/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test1090
+//
+//  Created by Craig Bennett on 8/8/15.
+//  Copyright (c) 2015 VoxelWise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool
+    {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

9d86db4a664b70a46db965d295fc8798a27838d7
Craig Bennett <craigo@gmail.com>
2015-08-17 22:14:29 -0700

    Manager initial commit

---
+++ Test1090/AircraftManager.h
@@ -0,0 +1,13 @@
+//
+//  P1090-AircraftManager.h
+//  Test1090
+//
+//  Created by Craig Bennett on 8/8/15.
+//  Copyright (c) 2015 VoxelWise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface P1090_AircraftManager : NSObject
+
+@end
---
+++ Test1090/AircraftManager.m
@@ -0,0 +1,13 @@
+//
+//  P1090-AircraftManager.m
+//  Test1090
+//
+//  Created by Craig Bennett on 8/8/15.
+//  Copyright (c) 2015 VoxelWise. All rights reserved.
+//
+
+#import "P1090-AircraftManager.h"
+
+@implementation P1090_AircraftManager
+
+@end

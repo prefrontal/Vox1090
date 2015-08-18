bac21532e8bd20ddba70483727d60a9e9a18c44c
Craig Bennett <prefrontal@gmail.com>
2015-08-17 22:49:56 -0700

    Misc project files commit

---
+++ Test1090/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Test1090
+//
+//  Created by Craig Bennett on 8/8/15.
+//  Copyright (c) 2015 VoxelWise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+    @property (strong, nonatomic) UIWindow *window;
+
+@end
+
